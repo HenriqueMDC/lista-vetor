9566fa3040a39c3f7f6c5c61aae3fe8fddb5749d
Henque Mateus Dalfovo Campos <hmdcampos@gmail.com>
2018-04-24 17:05:59 -0300

    Desenvolvimento do exercicio 04 com vetor
    solicitar a disciplina e as 4 notas, fazer a media e depois apresentar.

--- ListaVetor/Exercicio04.cs
+++ ListaVetor/Exercicio04.cs
@@ -8,5 +8,28 @@ namespace ListaVetor
 {
     class Exercicio04
     {
+        public Exercicio04()
+        {
+            double[] notas = new double[4];
+
+            Console.WriteLine("Disciplina: ");
+            string disciplina = Console.ReadLine();
+
+            Console.WriteLine("Nota 01: ");
+            notas[0] = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Nota 02: ");
+            notas[1] = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Nota 03: ");
+            notas[2] = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Nota 04: ");
+            notas[3] = Convert.ToDouble(Console.ReadLine());
+
+            double media = (notas[0] + notas[1] + notas[2] + notas[3]) / 4;
+
+            Console.WriteLine("Sua média é: " + media);
+        }
     }
 }
